1cc86160a1c856919c4b55de03f57f982b1c62d3
jeffondev <jeffondev@gmail.com>
2023-10-17 21:28:47 +0900

    write page little

--- src/routes/write.tsx
+++ src/routes/write.tsx
@@ -1,7 +1,28 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+const InputField = styled.input`
+  border: none;
+  width: 100%;
+`;
 export default function Write(){
+    const [inputValue, setInputValue] = useState('');
+    const onSubmit = () => {
+        //글 저장
+        
+        
+
+        setInputValue('');
+    }
     return(
         <>
-        write
+            <InputField
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={onSubmit}>저장</button>
         </>
     )
+    
 }
